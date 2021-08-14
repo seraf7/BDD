774c22ba47120ffdc687c4d3af0ad349a2a6cb06
seraf7 <zaske_4@hotmail.com>
2021-08-13 22:53:17 -0500

    Organización en directorios

--- Proyecto-Final/s-03-netmax-main-ddl.sql
+++ Proyecto-Final/s-03-netmax-main-ddl.sql
@@ -9,25 +9,25 @@ PROMPT =====================================
 PROMPT Creando fragmentos para sclbd_s1
 PROMPT =====================================
 CONNECT netmax_bdd/netmax_bdd@sclbd_s1
-@s-03-netmax-sclbd-s1-ddl.sql
+@ddl/s-03-netmax-sclbd-s1-ddl.sql
 
 PROMPT =====================================
 PROMPT Creando fragmentos para sclbd_s2
 PROMPT =====================================
 CONNECT netmax_bdd/netmax_bdd@sclbd_s2
-@s-03-netmax-sclbd-s2-ddl.sql
+@ddl/s-03-netmax-sclbd-s2-ddl.sql
 
 PROMPT =====================================
 PROMPT Creando fragmentos para iclbd_s1
 PROMPT =====================================
 CONNECT netmax_bdd/netmax_bdd@iclbd_s1
-@s-03-netmax-iclbd-s1-ddl.sql
+@ddl/s-03-netmax-iclbd-s1-ddl.sql
 
 PROMPT =====================================
 PROMPT Creando fragmentos para iclbd_s2
 PROMPT =====================================
 CONNECT netmax_bdd/netmax_bdd@iclbd_s2
-@s-03-netmax-iclbd-s2-ddl.sql
+@ddl/s-03-netmax-iclbd-s2-ddl.sql
 
 PROMPT ¡¡Listo!!
 EXIT
