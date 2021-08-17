a0c542a2fd01201e7254220d60af12e661156138
seraf7 <zaske_4@hotmail.com>
2021-08-17 00:16:41 -0500

    Datos para la carga inicial en la BDD

---
+++ Proyecto-Final/carga-inicial/netmax-carga-inicial-status-programa.sql
@@ -0,0 +1,8 @@
+insert into status_programa(status_programa_id,clave,descripcion) values(1,'PROGRAMADO','El programa está en la base de datos pero aun no disponible para consumo');
+insert into status_programa(status_programa_id,clave,descripcion) values(2,'DISPONIBLE','El cliente puede hacer streaming sobre el');
+insert into status_programa(status_programa_id,clave,descripcion) values(3,'SATURADO','Programa disponible con una gran demanda.');
+insert into status_programa(status_programa_id,clave,descripcion) values(4,'NO DISPONIBLE','El cliente ya no puede hacer streaming');
+insert into status_programa(status_programa_id,clave,descripcion) values(5,'OBSOLETO','Programa que ya concluyó con su ciclo de vida');
+
+
+
---
+++ Proyecto-Final/carga-inicial/netmax-carga-inicial-tipo-cuenta.sql
@@ -0,0 +1,4 @@
+insert into tipo_cuenta(tipo_cuenta_id,clave,descripcion,costo_mensual) values(1,'BASICA','Cuenta básica  gratuita',0.00);
+insert into tipo_cuenta(tipo_cuenta_id,clave,descripcion,costo_mensual) values(2,'PROFESIONAL','Cuenta enfocada al desarrollo profesional',20.00);
+insert into tipo_cuenta(tipo_cuenta_id,clave,descripcion,costo_mensual) values(3,'VIP','Cuenta con los mayores beneficios, genera cargos',150.45);
+
